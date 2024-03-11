159dc588f3578af735de7245d470bc53229e5abc
wangmaosong <wangmsd@dcits.com>
2024-03-11 16:43:42 +0800

    feat: 初始化

---
+++ pandas_eureka/src/main/java/com/wxf/eureka/EurekaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.wxf.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Eureka 注册中心
+ *
+ * @author Wxf
+ * @since 2024-03-11 16:31:12
+ **/
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
