b19017e79ec91a198a86f03ba248f1a8c1e548b8
wangmaosong <wangmsd@dcits.com>
2024-03-11 20:04:38 +0800

    feat: 网关

--- pandas_eureka/src/main/java/com/wxf/eureka/EurekaApplication.java
+++ pandas_eureka/src/main/java/com/wxf/eureka/EurekaApplication.java
@@ -12,9 +12,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  **/
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServiceApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
---
+++ pandas_gateway/src/main/java/com/wxf/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.wxf.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务
+ *
+ * @author Wxf
+ * @since 2024-03-11 19:54:21
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
