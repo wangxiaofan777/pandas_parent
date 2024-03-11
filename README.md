a39ed24532314d6e4981a11ebcb38a711ea20c5b
wangmaosong <wangmsd@dcits.com>
2024-03-11 19:48:14 +0800

    feat: 注册中心Eureka

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pandas_parent
 微服务2.x的版本
+
+### 1. 版本
+ * Spring Boot： 2.7.18
+ * Spring Cloud：2021.0.9
+
+
+### 2. 
--- pandas_eureka/src/main/java/com/wxf/eureka/EurekaServiceApplication.java
+++ pandas_eureka/src/main/java/com/wxf/eureka/EurekaServiceApplication.java
@@ -2,6 +2,7 @@ package com.wxf.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Eureka 注册中心
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Wxf
  * @since 2024-03-11 16:31:12
  **/
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
