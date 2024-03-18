25ad7f4af352c6f93335462fd1f0719b575ff733
wangmaosong <wangmsd@dcits.com>
2024-03-18 10:34:06 +0800

    feat: 添加两个模块 eureka集群化

---
+++ pandas-order/src/main/java/com/wxf/order/order/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.wxf.order.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 订单服务入口
+ *
+ * @author Wxf
+ * @since 2024-03-18 09:01:21
+ **/
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
---
+++ pandas-user/src/main/java/com/wxf/user/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.wxf.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户服务入口
+ *
+ * @author Wxf
+ * @since 2024-03-18 09:00:52
+ **/
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
